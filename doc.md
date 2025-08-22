ecff71767c5d392a086826642daeaed0ba71e5a2
rishirokade <rishikeshrokade7@gmail.com>
2025-08-22 12:28:41 +0530

    route order matter

--- doc.md
+++ doc.md
@@ -1,3 +1,5 @@
 1. npm init
 2. create src/app.js
 3. npm i express
+4. install nodemon
+5. update package.json
--- src/app.js
+++ src/app.js
@@ -2,8 +2,21 @@ const express = require("express");
 
 const app = express();
 
+// order of route matter
 app.use("/test", (req, resp) => {
-    resp.send("Hello World");
+    resp.send("Hello from test");
+});
+
+app.use("/hello/2", (req, resp) => {
+    resp.send("Hello hello/2");
+});
+
+app.use("/hello", (req, resp) => {
+    resp.send("Hello hello");
+});
+
+app.use("/", (req, resp) => {
+    resp.send("Hello from /");
 });
 
 app.listen(3000, () => {
